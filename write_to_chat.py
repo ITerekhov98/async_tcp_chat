822d0d42824619c965aa238dd55f1c626de13f94
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-08-23 16:03:29 +0300

    Add check for broken token

--- write_to_chat.py
+++ write_to_chat.py
@@ -1,7 +1,8 @@
 import asyncio
 from environs import Env
 import logging
-
+import json
+import aiofiles
 
 logger = logging.getLogger(__name__)
 
@@ -13,35 +14,61 @@ async def receive_response(reader: asyncio.StreamReader):
     return decoded_response
 
 
+async def register(reader: asyncio.StreamReader, writer:asyncio.StreamWriter):
+    await reader.readline()
+    user_name = input()
+    writer.write(f'{user_name}\n'.encode())
+    raw_user_info = await receive_response(reader)
+    user_info = json.loads(raw_user_info)
+
+    async with aiofiles.open('.env', 'a') as f:
+        await f.write(f"CHAT_HASH_ID={user_info['account_hash']}")
+
+    logger.info(f"user {user_info['nickname']} successfully register")
+    print(f"Добро пожаловать, {user_info['nickname']}!\r\n")
+    return True
+
+
 async def login(reader: asyncio.StreamReader, writer:asyncio.StreamWriter, chat_hash_id):
-    
     response = await receive_response(reader)
     if response.startswith('Hello'):
         writer.write(f'{chat_hash_id}\n'.encode())
 
-    user_info = await receive_response(reader)
+    raw_user_info = await receive_response(reader)
+    user_info = json.loads(raw_user_info)
+    if not user_info:
+        logger.warning('Cannot log in. Broken token.')
+        print(
+            'Не можем идентифицировать токен. '
+            'Проверьте правильность указанного токена и перезапустите программу,\r\n'
+            'либо укажите никнейм для повторной регистрации:\r\n'
+        )
+        return await register(reader, writer)
 
     response = await receive_response(reader)
     if response.startswith('Welcome'):
-        return True
+        return True  
     return False
+
 
 async def handle_writing(chat_hash_id):
     reader, writer = await asyncio.open_connection(
         'minechat.dvmn.org', 5050)
 
-    if await login(reader, writer, chat_hash_id):
-        while True:
-            message = input('Введите текст сообщения:\r\n')
-            writer.write(f'{message}\n\n'.encode())
-            await writer.drain()
-            print('Ваше сообщение отправлено!')
-            logger.info(f'Отправлено сообщение: {message}')
-            await receive_response(reader)
+    is_login = await login(reader, writer, chat_hash_id)
+    if not is_login:
+        logger.warning('Error occurred while trying to log in')
+        print('Не можем подключиться к чату. Попробуйте чуть позже')
+        writer.close()
+        return
 
-
-    writer.close()
-
+    while True:
+        message = input('Введите текст сообщения:\r\n')
+        writer.write(f'{message}\n\n'.encode())
+        await writer.drain()
+        print('Ваше сообщение отправлено!')
+        logger.info(f'Отправлено сообщение: {message}')
+        await receive_response(reader)
 
 
 if __name__ == '__main__':
@@ -53,5 +80,5 @@ if __name__ == '__main__':
     )
     env = Env()
     env.read_env()
-    chat_hash_id = env.str('CHAT_HASH_ID')
+    chat_hash_id = env.str('CHAT_HASH_ID', None)
     asyncio.run(handle_writing(chat_hash_id))
